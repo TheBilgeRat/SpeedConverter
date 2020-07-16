8a60cf48e65da35952851fb11f3906d8177735c8
Darren Couch <dcouch@gmail.com>
2020-07-16 16:41:26 -0700

    added testcases to main; finished BarkingDog

--- src/BarkingDog.java
+++ src/BarkingDog.java
@@ -1,2 +1,8 @@
 public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if(hourOfDay < 0 || hourOfDay > 23 || !barking)
+            return false;
+
+        return hourOfDay < 8 || hourOfDay > 22;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        SpeedConverter.printConversion(100.00d);
         MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        System.out.println(BarkingDog.shouldWakeUp(true, 1));
+        System.out.println(BarkingDog.shouldWakeUp(false, 2));
+        System.out.println(BarkingDog.shouldWakeUp(true, 8));
+        System.out.println(BarkingDog.shouldWakeUp(true, -1));
     }
 
 }
