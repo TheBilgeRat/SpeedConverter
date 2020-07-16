65afbfd9a0918bba284fa6bc4fd5095b4ecd8a6f
Darren Couch <dcouch@gmail.com>
2020-07-16 16:53:24 -0700

    add LeapYear tests

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
+import java.sql.SQLOutput;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("==================================================== SpeedConverter");
         SpeedConverter.printConversion(100.00d);
+        System.out.println("==================================================== MegabytesConverter");
         MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        System.out.println("==================================================== BarkingDog");
         System.out.println(BarkingDog.shouldWakeUp(true, 1));
         System.out.println(BarkingDog.shouldWakeUp(false, 2));
         System.out.println(BarkingDog.shouldWakeUp(true, 8));
         System.out.println(BarkingDog.shouldWakeUp(true, -1));
+        System.out.println("==================================================== LeapYear");
+        System.out.println(LeapYear.isLeapYear(-1600));
+        System.out.println(LeapYear.isLeapYear(1600));
+        System.out.println(LeapYear.isLeapYear(2017));
+        System.out.println(LeapYear.isLeapYear(2000));
     }
 
 }
