ffe47ea55be97545fa39d3ba0286397ecee243b3
Darren Couch <dcouch@gmail.com>
2020-07-16 17:00:44 -0700

    add decimal comparator and tests

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,11 @@ public class Main {
         System.out.println(LeapYear.isLeapYear(1600));
         System.out.println(LeapYear.isLeapYear(2017));
         System.out.println(LeapYear.isLeapYear(2000));
+        System.out.println("==================================================== DecimalComparator");
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123));
     }
 
 }
