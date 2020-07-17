3215433b373dce59a82fa3a120738cccb4add401
Darren Couch <dcouch@gmail.com>
2020-07-16 17:14:20 -0700

    add Equal Sum Checker and tests

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,10 @@ public class Main {
         System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175, 3.176));
         System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0, 3.0));
         System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123, 3.123));
+        System.out.println("==================================================== EqualSumChecker");
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 1));
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 2));
+        System.out.println(EqualSumChecker.hasEqualSum(1, -1, 0));
     }
 
 }
