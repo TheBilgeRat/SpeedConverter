142eb576af97033886887f11d3fbffb58b9b95bc
Darren Couch <dcouch@gmail.com>
2020-07-17 16:44:13 -0700

    fixed bugs, added testcases

--- src/Main.java
+++ src/Main.java
@@ -82,6 +82,10 @@ public class Main {
         System.out.println(SumOddRange.sumOdd(13, 13));
         System.out.println(SumOddRange.sumOdd(100, -100));
         System.out.println(SumOddRange.sumOdd(100, 1000));
+        System.out.println("==================================================== NumberPalindrome");
+        System.out.println(NumberPalindrome.isPalindrome(-1221));
+        System.out.println(NumberPalindrome.isPalindrome(707));
+        System.out.println(NumberPalindrome.isPalindrome(11212));
     }
 
 }
--- src/NumberPalindrome.java
+++ src/NumberPalindrome.java
@@ -1,15 +1,16 @@
 public class NumberPalindrome {
     public static boolean isPalindrome(int number) {
         int reverse = 0;
-        int numCopy = number;
         number = Math.abs(number);
+        int numCopy = number;
 
         while (number > 0) {
             int lastDigit = number % 10;
             reverse *= 10;
             reverse += lastDigit;
-            number = number % 10;
+            number = number / 10;
         }
+
         return reverse == numCopy;
     }
 }
