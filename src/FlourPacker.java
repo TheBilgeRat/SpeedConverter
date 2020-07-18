6577aadf4c00b8a601768deac9046f7df4154d3d
Darren Couch <dcouch@gmail.com>
2020-07-18 12:17:18 -0700

    changed logic.

--- src/FlourPacker.java
+++ src/FlourPacker.java
@@ -2,11 +2,11 @@ public class FlourPacker {
     public static boolean canPack(int bigCount, int smallCount, int goal) {
         // big: 5kg, small: 1kg, goal amount of kilos to assemble a package
         // Case 1: Negatives in any number or too many big bags:
-        if(bigCount < 0 || smallCount < 0 || goal < 0 || ((bigCount * 5) > goal && smallCount == 0)) {
+        if(bigCount < 0 || smallCount < 0 || goal < 0 || ((bigCount * 5)  + smallCount < goal)) {
             return false;
         }
 
         // Case 2: verifying that we have enough flour to go around
-        return ((bigCount * 5) + smallCount >= goal || bigCount == 0 && smallCount >= goal);
+        return (smallCount >= goal % 5);
     }
 }
