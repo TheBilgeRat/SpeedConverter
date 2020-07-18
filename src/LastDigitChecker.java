cf2799d1761e51ce2d3d175085b370c0285f34e6
Darren Couch <dcouch@gmail.com>
2020-07-17 21:14:01 -0700

    LastDigitChecker initial checkin

---
+++ src/LastDigitChecker.java
@@ -0,0 +1,8 @@
+public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int a, int b, int c) {
+        if(a < 10 || b < 10 || c < 10 || a > 1000 || b > 1000 || c > 1000) {
+            return false;
+        }
+        return (a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10);
+    }
+}
